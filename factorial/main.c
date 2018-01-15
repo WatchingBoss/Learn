064844e5b71fc5d71a7f72ce22dc198a7e6a5c88
WatchingBoss <opil94@yahoo.com>
2018-01-15 13:35:50 +0300

    Create new function for computing factorial number's

---
+++ factorial/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fact(int x){
+    if (x == 1) return 1;
+    return x * fact (x - 1);
+}
+
+int main() {
+    int num;
+    puts("Enter number for computing factorial");
+    scanf("%i", &num);
+    printf("Factorial of %i is: %i\n", num, fact(num));
+    return 0;
+}
