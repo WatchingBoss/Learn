1e24cf8f6a2bf8de3c89606237f647851d34bc1a
WatchingBoss <opil94@yandex.ru>
2018-02-15 21:18:38 +0300

    Exercises of chapter 3

--- Linux/Let_us_C/Chapter_3/exercise.c
+++ Linux/Let_us_C/Chapter_3/exercise.c
@@ -249,19 +249,230 @@ void BG()
     printf("%d in decimal is %d in octal\n", number, octal);
 }*/
 
-void BH(long number)
+void BH(int number)
 {
-    long n;
-    int octal[100], i = 1, j;
+    int n = number;
+    int octal[5], i = 1, j;
     while (n != 0)
     {
         octal[i++] = n % 8;
+        n /= 8;
+    }
+    printf("The %i in decimal is ", number);
+    for (j = i - 1; j > 0; j--)
+        printf("%d", octal[j]);
+    puts(" in octal");
+}
 
+/* Exercise i
+ * Program to find the range of a set of numbers*/
+void BI()
+{
+    int min = 32767, max = -32768, num, range;
+    char ans;
+    do {
+        printf("Enter number\n");
+        scanf("%d", &num);
+        if (num > max)
+            max = num;
+        if (num < min)
+            min = num;
+        printf("Do you want to enter another number? y/n\n");
+        fgetc(stdin);
+        scanf("%c", &ans);
+    } while (ans == 'y' || ans == 'Y');
+    range = max - min;
+    printf("Range between %d and %d is %d", min, max, range);
+}
+
+/* E. Attempt the following*/
+/* Exercise a
+ * Program to print all prime numbers from 1 to n*/
+void EA()
+{
+    int num, max, check = 2;
+    printf("Enter first and last numbers");
+    scanf("%d %d", &num, &max);
+    while (num <= max)
+    {
+        while (check < num)
+        {
+            if (num % check == 0 && check < num)
+            {
+                break;
+            }
+            check++;
+        }
+        if (check == num)
+            printf("%d is prime number\n", num);
+        num++;
+        check = 2;
+    }
+}
+
+/* Exercise b
+ * Program to fill the entire screen with a smiling face*/
+void EB()
+{
+    char smile = 1;
+    for (int i = 1; i < 15; i++)
+    {
+        for (int f = 1; f < 100; f++)
+            printf("%c", smile);
+        puts("");
+    }
+}
+
+
+/* Exercise c
+* Program to add dirst seven terms of the following series
+* 1/1! + 2/2! + 3/3! + ...*/
+void EC()
+{
+    float f;
+    float sum = 0;
+    for (int i = 1; i <= 7; i++)
+    {
+        f = 1;
+        for (int n = i; n > 0; n--)
+        {
+            f *= n;
+        }
+        printf("%d/%.0f is %.5f\n", i, f, i / f);
+        sum += i / f;
+    }
+    printf("Result is %.5f\n", sum);
+}
+
+/* Exercise d
+ * Program to generate all combinations of 1, 2 and 3*/
+/*void EC()
+{
+    int z, x, c;
+    for (z = 1; z <= 3; z++)
+    {
+        for (x = 1; x <= 3; x++)
+        {
+            for (c = 1; c <= 3; c++)
+            {
+                if (z != x && z != c && x != c)
+                    printf("%d%d%d\n", z, x, c);
+            }
+        }
+    }
+}*/
+void print(int *num, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d", num[i]);
+    printf("\n");
+}
+void ED()
+{
+    int num[3], temp, i, n, j;
+    printf("How many numbers you want to enter: ");
+    scanf("%d", &n);
+    printf("Enter a list of numbers to see all combinations: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &num[i]);
+    for (j = 1; j <= n; j++)
+    {
+        for (i = 0; i < n - 1; i++)
+        {
+            temp = num[i];
+            num[i] = num[i + 1];
+            num[i + 1] = temp;
+            print(num, n);
+            for (int k = 0; k < n; k++)
+                printf("%d", num[k]);
+            printf("\n");
+        }
+    }
+}
+
+/* Exercise e
+ * Program to produce a table of values of formula*/
+void EE()
+{
+    float i, x;
+    int j, y;
+    puts("--------------------------------");
+    for (y = 1; y <= 6; y++)
+    {
+        x = 5.5;
+        for (j = 1; j <= 15; j++)
+        {
+            i = 2 + (y + 0.5f * x);
+            printf("| i = %4.1f | y = %2d | x = %4.1f |\n", i, y, x);
+            x += 0.5;
+        }
+        puts("--------------------------------");
+    }
+
+}
+
+/* Exercise f
+ * Program to produce figure from alphabets*/
+void EF()
+{
+    char letter[] = {65, 66, 67, 68, 69, 70, 71};
+    int alphabet;
+    for (int l = 6; l > 0; l--)
+    {
+        for (int i = 0; i <= 6; i++)
+            printf("%c ", letter[i]);
+        for (int i = 5; i >= 0; i--)
+            printf("%c ", letter[i]);
+        puts("");
+        letter[l] = ' ';
+    }
+    for (int l = 0; l <= 7; l++)
+    {
+        for (int i = 0; i <= 6; i++)
+            printf("%c ", letter[i]);
+        for (int i = 5; i >= 0; i--)
+            printf("%c ", letter[i]);
+        puts("");
+        alphabet = 65 + l;
+        letter[l] = alphabet ;
+    }
+}
+
+/* Exercise g
+ * Program to fill the entire screen with alphabets diamond and heart*/
+void EG()
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int d = 0; d < 2; d++)
+        {
+            for (int s = 6 - i; s > 0; s--)
+                printf("%c ", 3);
+            for (int s = i * 4; s > 0; s--)
+                printf("%c", ' ');
+            for (int s = 6 - i; s > 0; s--)
+                printf("%c ", 3);
+        }
+        puts("");
+    }
+    for (int i = 5; i >= 0; i--)
+    {
+        for (int d = 0; d < 2; d++)
+        {
+            for (int s = 6 - i; s > 0; s--)
+                printf("%c ", 4);
+            for (int s = i * 4; s > 0; s--)
+                printf("%c", ' ');
+            for (int s = 6 - i; s > 0; s--)
+                printf("%c ", 4);
+        }
+        puts("");
     }
 }
 
 int main()
 {
-    BH(4096);
+    EG();
     return 0;
 }
