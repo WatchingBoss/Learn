4ff8875590ccf186a5c1dec2fae207a9b578e15e
WatchingBoss <opil94@yandex.ru>
2018-02-19 15:47:28 +0300

    New

---
+++ Linux/Let_us_C/Chapter_4/cmake-build-debug/CMakeFiles/exercise.c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise.c.dir/exercise.c.obj"
+  "exercise.c.pdb"
+  "exercise.c.exe"
+  "exercise.c.exe.manifest"
+  "libexercise.c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exercise.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ fibonacci/cmake-build-debug/CMakeFiles/fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibonacci.dir/main.c.obj"
+  "fibonacci.pdb"
+  "fibonacci.exe"
+  "fibonacci.exe.manifest"
+  "libfibonacci.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
