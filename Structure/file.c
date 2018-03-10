d2ad2403fedf5ceb6c7be3663a0ed967b511a3a0
WatchingBoss <opil94@yandex.ru>
2018-03-10 21:55:50 +0300

    struct

---
+++ Structure/file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct exercise
+{
+	
+};
+
+struct day
+{
+	
+};
+
+struct program
+{
+	
+};
+
+int main()
+{
+    
+    return 0;
+}
+
+
+/*
+
+program -> day -> exercise
+
+*/
