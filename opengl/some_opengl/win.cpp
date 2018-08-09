8adfc29368abf24bbb432d743de805fbcd6e8998
WatchingBoss <opil94@yandex.ru>
2018-08-09 21:05:30 +0300

    Same triangle for now))

--- opengl/some_opengl/win.cpp
+++ opengl/some_opengl/win.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <iostream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -24,6 +26,7 @@ GLFWwindow * create_new_window(const char *title = "Default", int w = 1024, int 
 	return new_win;
 }
 
+# define STEP 2
 void mainWin()
 {
 	if(!glfwInit())
@@ -33,15 +36,36 @@ void mainWin()
 
 	glfwMakeContextCurrent(win);
 
+	if(glewInit() != GLEW_OK)
+		sys_error("mainWin: glewInit error");
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+#if STEP == 2
+	float vertex_position[] = { -0.5f, -0.5f,
+								 0.0f,  0.5f,
+								 0.5f, -0.5f };
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof vertex_position, vertex_position, GL_STATIC_DRAW);
+#endif
+
 	while(glfwGetKey(win, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(win))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
+#if STEP == 2
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+#endif
+
+#if STEP == 1
 		glBegin(GL_TRIANGLES);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f( 0.0f,  0.5f);
 		glVertex2f( 0.5f, -0.5f);
 		glEnd();
+#endif
 
 		glfwSwapBuffers(win);
 
