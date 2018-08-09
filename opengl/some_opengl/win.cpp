72a8e30de65d5d7370e5e2964beb75cc1eb7b962
WatchingBoss <opil94@yandex.ru>
2018-08-09 14:25:53 +0300

    Sample from video TheChernoProject, just draw triangle. No any runtime errrors. Why there are run time errros with other two examples?

---
+++ opengl/some_opengl/win.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void sys_error(const char *e)
+{
+	glfwTerminate();
+
+	perror(e);
+	exit(EXIT_FAILURE);
+}
+
+GLFWwindow * create_new_window(const char *title = "Default", int w = 1024, int h = 768, 
+							   GLFWmonitor *monitor = nullptr, GLFWwindow *share = nullptr)
+{
+	GLFWwindow *new_win = glfwCreateWindow(w, h, title, monitor, share);
+	if(!new_win)
+	{
+		glfwDestroyWindow(new_win);
+		sys_error("create_new_window: glfwCreateWindow error");
+	}
+	return new_win;
+}
+
+void mainWin()
+{
+	if(!glfwInit())
+		sys_error("mainWin: glfwInit error");
+
+	GLFWwindow *win = create_new_window("My main window");
+
+	glfwMakeContextCurrent(win);
+
+	while(glfwGetKey(win, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(win))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glEnd();
+
+		glfwSwapBuffers(win);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
+
+int main()
+{
+	mainWin();
+
+	return 1;
+}
