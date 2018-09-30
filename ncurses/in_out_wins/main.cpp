5fefff2cd441dc5b8b8edfc404fb99b9e44c7d8a
WatchingBoss <opil94@yandex.ru>
2018-09-30 15:19:22 +0300

    Display title, get name, set colors

--- ncurses/in_out_wins/main.cpp
+++ ncurses/in_out_wins/main.cpp
@@ -2,11 +2,12 @@
 #include <ctime>
 
 #include <array>
+#include <list>
+#include <queue>
+
 #include <chrono>
 #include <iostream>
-#include <list>
 #include <memory>
-#include <queue>
 #include <string>
 
 #include <mutex>
@@ -15,6 +16,11 @@
 #include <ncurses.h>
 
 #define DELAY 1000
+
+#define USE_COLOR(win, pair, x)                                                \
+	wattron(win, COLOR_PAIR(pair));                                            \
+	x;                                                                         \
+	wattroff(win, COLOR_PAIR(pair))
 
 using str_ptr   = std::shared_ptr<std::string>;
 using str_q_ptr = std::shared_ptr<std::queue<str_ptr>>;
@@ -56,7 +62,7 @@ WINDOW *create_new_win(win_param &param) {
 
 void cleanWin(WINDOW *win, int bottom, int top, const win_param &param) {
 	const int bufferSize = param.width - 2;
-	char      buffer[param.width - 2];
+	char      buffer[bufferSize];
 	memset(buffer, ' ', bufferSize);
 	for (int i = bottom; i >= top; --i)
 		mvwaddnstr(win, i, 1, buffer, bufferSize);
@@ -83,7 +89,10 @@ void get_user_input(WINDOW *win, win_param win_p) {
 		char mesBuffer[mesBufferSize]   = { 0 },
 		     timeBuffer[timeBufferSize] = { 0 }, buffer[bufferSize] = { 0 };
 
+		mtx.lock();
 		mvwgetnstr(win, input_line, 1, mesBuffer, mesBufferSize);
+		mtx.unlock();
+
 		cleanWin(win, bottom_line, top_line, win_p);
 
 		save_current_time(timeBuffer, timeBufferSize);
@@ -96,11 +105,11 @@ void get_user_input(WINDOW *win, win_param win_p) {
 }
 
 void show_output(WINDOW *win, win_param win_p) {
-	const int top_line = 3, bottom_line = win_p.height - top_line;
+	const int height = win_p.height, top_line = 3, bottom_line = height - 2;
 	while (PROG_RUN) {
 		if (inputQueue->empty()) continue;
 
-		int     output_line  = win_p.height - 2;
+		int     output_line  = bottom_line;
 		str_ptr new_mes      = inputQueue->front();
 		size_t  dispMesCount = 0;
 
@@ -108,55 +117,83 @@ void show_output(WINDOW *win, win_param win_p) {
 
 		if (!dispInputs->empty()) dispMesCount = dispInputs->size();
 
-		mvwaddnstr(win, output_line, 1, new_mes->c_str(), new_mes->length());
+		USE_COLOR(win, 2,
+		          mvwaddnstr(win, output_line, 1, new_mes->c_str(),
+		                     new_mes->length()));
 
-		mtx.lock();
-		for (str_ptr &s : *dispInputs)
-			mvwaddnstr(win, --output_line, 1, s->c_str(), s->length());
-		mtx.unlock();
+		USE_COLOR(win, 3,
+		          for (const str_ptr &s
+		               : *dispInputs) mvwaddnstr(win, --output_line, 1,
+		                                         s->c_str(), s->length()));
 
-		if (dispMesCount >= static_cast<size_t>(bottom_line))
-			dispInputs->pop_front();
+		if (dispMesCount >= static_cast<size_t>(height - 5))
+			dispInputs->pop_back();
 
-		dispInputs->push_back(new_mes);
+		dispInputs->push_front(new_mes);
 		inputQueue->pop();
 
 		wrefresh(win);
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(DELAY));
 	}
 }
 
 std::array<std::thread, 2> define_threads(WINDOW *in, WINDOW *out,
                                           win_param in_p, win_param out_p) {
-	std::array<std::thread, 2> ths;
-	std::thread                input(get_user_input, in, in_p);
-	std::thread                output(show_output, out, out_p);
-	ths.at(0) = (std::move(input));
-	ths.at(1) = (std::move(output));
+	std::thread input(get_user_input, in, in_p);
+	std::thread output(show_output, out, out_p);
+
+	std::array<std::thread, 2> ths = { std::move(input), std::move(output) };
 	return ths;
 }
 
+void displayWinTitle(WINDOW *win, win_param &win_p)
+{
+	const char *title = "Display user inputs";
+	const size_t titleSize = strlen(title);
+	int x = win_p.width / 2 - titleSize / 2, y = 2;
+
+	USE_COLOR(win, 1, mvwprintw(win, y, x, title, titleSize));
+
+	wrefresh(win);
+}
+
 void prog() {
+	initscr();
+	start_color();
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);
+
 	win_param output_win, input_win;
 	define_params(output_win, input_win);
 
 	WINDOW *prog_in  = create_new_win(input_win);
 	WINDOW *prog_out = create_new_win(output_win);
 
+	displayWinTitle(prog_out, output_win);
+
 	std::array<std::thread, 2> threads =
 	    define_threads(prog_in, prog_out, input_win, output_win);
 
 	for (std::thread &th : threads)
 		if (th.joinable()) th.join();
+
+	endwin();
+}
+
+void getUserName(){
+	std::string userName;
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, userName);
+	str_ptr message(new std::string("Welcome " + userName + "!"));
+	inputQueue->push(message);
 }
 
 int main() {
-	initscr();
+	getUserName();
 
-	prog();
+	std::thread p(prog);
 
-	endwin();
+	if(p.joinable()) p.join();
 
 	return 0;
 }
