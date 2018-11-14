b10b38a16304c1eb70e3430573b1e342efe02824
Watchingboss <opil94@yandex.ru>
2018-11-14 18:00:46 +0300

    Neu net sample

--- ml/neural_net_cpp/gen_train_data.py
+++ ml/neural_net_cpp/gen_train_data.py
@@ -1,5 +1,14 @@
+import random
+
 def gen_train_data():
-    print("Generating train data")
+    train_data = "data/in.txt"
+
+    with open(train_data) as train_file:
+        train_file.write("topology: 2 4 1")
+        for i in range(2000):
+            num_1, num_2 = random.randrange(0, 1), random.randrange(0, 1)
+            train_file.write("in: {}.0 {}.0\n\
+out: {}.0".format(num_1, num_2, num_1 ^ num_2))
 
 
 if __name__ == "__main__":
