50720015d5dcc636d90cd97bed3022f8785f710f
WatchingBoss <opil94@yandex.com>
2020-02-26 21:24:43 +0300

    TextBlock, Label and TextBox using samples

--- csharp/LearnWPF/LearnWPF/SecondWindow.xaml.cs
+++ csharp/LearnWPF/LearnWPF/SecondWindow.xaml.cs
@@ -1,15 +1,39 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace LearnWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SecondWindow.xaml
     /// </summary>
     public partial class SecondWindow : Window
     {
+
+        private bool FirstFocus = true;
+
         public SecondWindow()
         {
             InitializeComponent();
+        }
+
+        private void tbLabelKeyAccess_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            txtLabel.Text = tbLabelKeyAccess.Text;
+        }
+
+        private void tbLabelKeyAccess_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!FirstFocus) return;
+
+            tbLabelKeyAccess.Text = "";
+            FirstFocus = false;
+        }
+
+        private void tbLabelKeyAccess_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            GetSelectedText.Text = "Selected: \n" + txtBox.SelectedText;
         }
     }
 }
