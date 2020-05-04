748ef9eee9c21c7af4e5b1221feb09f0d9382244
WatchingBoss <opil94@yandex.com>
2020-05-04 20:04:42 +0300

    Using output, popup, fileBrowse

--- python/learn_pysimplegui/main.py
+++ python/learn_pysimplegui/main.py
@@ -1,14 +1,24 @@
 import PySimpleGUI as gui
+import os
+
+
+def get_text_from_file(file_path):
+    with open(file_path) as file:
+        text = file.read()
+
+    return text
 
 
 def first_window(theme, title):
     gui.theme(theme)
 
     layout = [
-        [gui.Text("Enter number"), gui.InputText()],
-        [gui.Text("Enter exponent"), gui.InputText()],
-        [gui.Text("Result"), gui.Output()],
-        [gui.Button("Calc"), gui.Button("Exit")]
+        [gui.Text("Enter number", text_color="lightgreen"), gui.InputText()],
+        [gui.Text("Enter exponent", text_color="cyan"), gui.InputText()],
+        [gui.Text("For text"), gui.Output(size=(100, 20))],
+        [gui.Input(), gui.FileBrowse("Browse your file")],
+        [gui.Button("Calc"), gui.Button("Popup"), gui.Button("Exit")],
+        [gui.Button("Open file")]
     ]
 
     window = gui.Window(title, layout)
@@ -19,6 +29,10 @@ def first_window(theme, title):
             break
         elif event in (None, "Calc"):
             print("{:.0f}".format(float(values[0]) ** float(values[1])))
+        elif event in (None, "Popup"):
+            gui.Popup(os.getcwd())
+        elif event in (None, "Open file"):
+            print("{}".format(get_text_from_file(values[2])))
 
     window.close()
 
