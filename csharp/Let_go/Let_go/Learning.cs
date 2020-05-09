052621a7f4de7781a1ae73ff5d98920f63f31681
WatchingBoss <opil94@yandex.com>
2020-05-09 11:10:47 +0300

    Using interface for properties

---
+++ csharp/Let_go/Let_go/Learning.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Let_go
+{
+    internal class Learning : ILearning {
+        private DateTime _Time;
+
+        public Learning() {
+            _Time = DateTime.Now;
+        }
+
+        public void RefreshTime() => _Time = DateTime.Now;
+
+        private string _currentDateTime;
+        public string CurrentDateTime {
+            get => _currentDateTime = _Time.ToString();
+        }
+        private long _currentTicks;
+        public long CurrentTicks {
+            get => _currentTicks = _Time.Ticks;
+        }
+    }
+
+    internal interface ILearning
+    {
+        string CurrentDateTime {
+            get;
+        }
+
+        long CurrentTicks {
+            get;
+        }
+    }
+}
--- csharp/Let_go/Let_go/MainWindow.xaml.cs
+++ csharp/Let_go/Let_go/MainWindow.xaml.cs
@@ -1,35 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
+using System.Threading;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Let_go
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
 
             PrintOutput();
         }
 
-        private void PrintOutput()
-        {
-            ProgramOutput.Text = "I'll print output here";
+        private void PrintOutput() {
+            Learning FirstInstance = new Learning();
+            FirstInstance.RefreshTime();
+            tbTopLeft.Text = $"Date and time:\n{FirstInstance.CurrentDateTime}\n" +
+                $"All ticks:\n{FirstInstance.CurrentTicks}";
         }
     }
 }
