6529e9a4f88a9dc0be82b29bb6f6bf8975a2733e
WatchingBoss <opil94@yandex.ru>
2020-05-10 15:47:59 +0300

    Chang pug and sass to html and scss

--- web/learn_1/gulpfile.js
+++ web/learn_1/gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const pug = require("gulp-pug");
 const del = require("del");
 const browser_sync = require("browser-sync").create();
 
@@ -18,64 +17,63 @@ const paths = {
     input: "src/",
     output: "dist/",
     scripts: {
-      input: "src/scripts/**/*.js",
-      output: "dist/scripts/"
+        input: "src/scripts/**/*.js",
+        output: "dist/scripts/"
     },
     styles: {
-        input: "src/sass/**/*.sass",
+        input: "src/styles/**/*.scss",
         output: "dist/css/"
     },
-    pug: {
-        input: "src/**/*.pug",
+    html: {
+        input: "src/**/*.html",
         output: "dist/",
     },
     reload: "./dist"
 };
 
-function browserSync(done){
+function browserSync(done) {
     browser_sync.init({
-        server:{ baseDir: paths.reload },
+        server: {baseDir: paths.reload},
         port: 8555
     });
     done();
 }
 
-function browserSyncReload(done){
+function browserSyncReload(done) {
     browser_sync.reload();
     done();
 }
 
-function sassTask(){
+function sassTask() {
     return gulp.src(paths.styles.input)
         .pipe(sass())
         .pipe(gulp.dest(paths.styles.output))
         .pipe(browser_sync.stream());
 }
 
-function pugTask(){
-    return gulp.src(paths.pug.input)
-        .pipe(pug())
-        .pipe(gulp.dest(paths.pug.output))
+function htmlTask() {
+    return gulp.src(paths.html.input)
+        .pipe(gulp.dest(paths.html.output))
         .pipe(browser_sync.stream());
 }
 
-function scriptTask(){
+function scriptTask() {
     return gulp.src(paths.scripts.input)
         .pipe(gulp.dest(paths.scripts.output))
         .pipe(browser_sync.stream());
 }
 
-function clean(){
+function clean() {
     return del([paths.output]);
 }
 
-function watchTask(){
-    gulp.watch(paths.pug.input, pugTask);
+function watchTask() {
+    gulp.watch(paths.html.input, htmlTask);
     gulp.watch(paths.styles.input, sassTask);
     gulp.watch(paths.scripts.input, scriptTask);
 }
 
-const build = gulp.series(clean, gulp.parallel(pugTask, sassTask, scriptTask));
+const build = gulp.series(clean, gulp.parallel(htmlTask, sassTask, scriptTask));
 const watch = gulp.parallel(watchTask, browserSync);
 
 exports.build = build;
