1921c26a5bbed951e38b6e4539e9e2870a0b2862
ActionLS <opil94@yandex.ru>
2021-05-19 21:37:22 +0300

    Using threading and concurrent.futures

---
+++ python/learning_1/main.py
@@ -0,0 +1,67 @@
+import concurrent.futures
+import threading
+import time
+
+
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    result = 1
+    for i in range(x, 1, -1):
+        result *= i
+    time.sleep(0.25)
+    print(f"Factorial of {x:-5} is {result}")
+
+
+def return_factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    result = 1
+    for i in range(x, 1, -1):
+        result *= i
+    time.sleep(0.25)
+    return f"Factorial of {x:-5} is {result}"
+
+
+def do_queue(start, end):
+    for i in range(start, end):
+        factorial(i**2)
+
+
+def do_threading(start, end):
+    my_threads = []
+
+    for i in range(start, end):
+        t = threading.Thread(target=factorial, args=(i**2,))
+        t.start()
+        my_threads.append(t)
+
+    for t in my_threads:
+        t.join()
+
+
+def do_concurrent(start, end):
+    with concurrent.futures.ThreadPoolExecutor() as ex:
+        results = [ex.submit(return_factorial, i**2) for i in range(start, end)]
+        for i in concurrent.futures.as_completed(results):
+            print(i.result())
+
+
+def compare(func):
+    start = time.perf_counter()
+    x = 5
+    func(x, x+5)
+    finish = time.perf_counter()
+
+    print(f"Used time: {finish-start:.5} seconds\n")
+
+
+if __name__ == "__main__":
+    print("Do queue")
+    compare(do_queue)
+
+    print("Do threading")
+    compare(do_threading)
+
+    print("Do concurrent")
+    compare(do_concurrent)
