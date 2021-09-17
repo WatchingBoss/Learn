6cc9aeb1f14d9617b408db66248164d1680ad4d8
ActionLS <opil94@yandex.ru>
2021-09-18 01:15:27 +0300

    Using data from finviz and tradingview to get buy signal

--- python/signals/main.py
+++ python/signals/main.py
@@ -1,10 +1,13 @@
-import os, sys, json, xlwt, time
+import os, sys, json, time
 from datetime import datetime, timedelta, timezone
+import asyncio
+import concurrent.futures
+
 import tinvest
 import numpy as np
 import pandas as pd
-import concurrent.futures
-import asyncio
+from tradingview_ta import TA_Handler, Interval, Exchange
+
 import support, instrument as inst
 from instrument import Stock
 
@@ -38,78 +41,8 @@ def get_market_stocks(client, stocks_dict):
     payload = client.get_market_stocks().payload
     stocks_usd = [stock for stock in payload.instruments[:] if stock.currency == 'USD']
 
-    max_high_price = 250
-    min_avg_month_high_low_diff = 0.12
-    min_avg_day_high_low_diff = 0.025
-    min_avg_volume = 100_000
-
-    now = datetime.now()
-    start_month = now - timedelta(30 * 3)
-    start_day = now - timedelta(20)
-
     for i in range(len(stocks_usd)):
         stock = stocks_usd[i]
-        try:
-            candles_month = client.get_market_candles(
-                figi=stock.figi, from_=start_month, to=now, interval=tinvest.CandleResolution.month
-            ).payload.candles
-
-            candles_day = client.get_market_candles(
-                figi=stock.figi, from_=start_day, to=now, interval=tinvest.CandleResolution.day
-            ).payload.candles
-
-        except tinvest.exceptions.TooManyRequestsError:
-            print("Waiting for 61 seconds")
-            time.sleep(61)
-
-            candles_month = client.get_market_candles(
-                figi=stock.figi, from_=start_month, to=now, interval=tinvest.CandleResolution.month
-            ).payload.candles
-
-            candles_day = client.get_market_candles(
-                figi=stock.figi, from_=start_day, to=now, interval=tinvest.CandleResolution.day
-            ).payload.candles
-
-        except tinvest.exceptions.UnexpectedError:
-            continue
-
-        print(f"{i}: {stock.ticker}")
-
-        if len(candles_month) < 2 or len(candles_day) < 2:
-            continue
-
-        try:
-            this_max_high = sorted([candle.h for candle in candles_month])[-1]
-            print(f"Max high: {this_max_high}")
-            if this_max_high > max_high_price:
-                continue
-        except IndexError:
-            print([candle.h for candle in candles_month])
-
-        try:
-            difference = np.mean([(candle.h - candle.l) / candle.l for candle in candles_month])
-            print(f"Average month high - low difference: {difference:.1%}")
-            if difference < min_avg_month_high_low_diff:
-                continue
-        except ZeroDivisionError:
-            continue
-
-        try:
-            difference = np.mean([(candle.h - candle.l) / candle.l for candle in candles_day])
-            print(f"Average day high - low difference: {difference:.1%}")
-            if difference < min_avg_day_high_low_diff:
-                continue
-        except ZeroDivisionError:
-            continue
-
-        try:
-            avg_volume = np.mean([candle.v for candle in candles_day])
-            print(f"Average 10 days volume: {avg_volume:.1f}")
-            if avg_volume < min_avg_volume:
-                continue
-        except ZeroDivisionError:
-            continue
-
         stocks_dict[stock.figi] = Stock(stock.ticker, stock.figi, stock.isin, stock.currency)
 
 
@@ -141,7 +74,7 @@ async def test_streaming(key, stock_dict):
 def get_candles(client, figi, tf: inst.Timeframe):
     now = datetime.now(tz=timezone.utc)
     candle_list = []
-    max_list = 200
+    max_list = 250
     for _ in range(10):
         if len(candle_list) >= max_list:
             break
@@ -155,35 +88,31 @@ def get_candles(client, figi, tf: inst.Timeframe):
         except tinvest.exceptions.TooManyRequestsError:
             time.sleep(60)
 
-    if len(candle_list) < 200:
+    if len(candle_list) < 250:
         max_list = len(candle_list)
 
     tf.candles = pd.DataFrame(
         candle_list, columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
-        ).sort_values(by='Time', ascending=False, ignore_index=True)[:max_list]
+        ).sort_values(by='Time', ascending=True, ignore_index=True)
 
 
 def fill_candles(client, stock_dict):
     for stock in stock_dict.values():
-        get_candles(client, stock.figi, stock.day)
-        get_candles(client, stock.figi, stock.hour)
+        # get_candles(client, stock.figi, stock.day)
+        # get_candles(client, stock.figi, stock.hour)
         get_candles(client, stock.figi, stock.m15)
 
 
 async def print_stock(stock_dict):
     for stock in stock_dict.values():
-        print(f"Interval: 15m\n"
-              f"{stock.m15.candles.loc[0].to_string()}\n"
-              f"Last time modify: {stock.m15.last_modify_time.time()}")
-        await stock.ema()
-        print(stock.m15.ema.to_string())
-
-
-# async def calc_ema(stock_dict):
-#     while True:
-#         for stock in stock_dict.values():
-#             await stock.ema()
-#         await asyncio.sleep(10)
+        tf = stock.m15
+        print(f"Ticker: {stock.ticker}\n"
+              f"Interval: {tf.interval}\n"
+              f"{tf.candles}\n"
+              f"Last time modify: {tf.last_modify_time.time()}")
+        tf.ema()
+        print(tf.candles)
+        # print(stock.m15.ema.to_string())
 
 
 async def main():
@@ -195,39 +124,50 @@ async def main():
 
     path_data_dir = os.path.join(os.curdir, 'data')
     path_stocks_usd = os.path.join(path_data_dir, 'stocks_usd' + '.json')
-
     if not os.path.isdir(path_data_dir):
         os.mkdir(path_data_dir)
 
-    stock_long = {}
+    all_stocks = {}
+    stock_long_only = {}
     stock_long_short = {}
 
     client = tinvest.SyncClient(keys['token_tinkoff_real'])
 
-    p = client.get_market_search_by_ticker('CNK').payload.instruments[0]
-    stock = Stock(ticker=p.ticker, figi=p.figi, isin=p.isin, currency=p.currency)
-    stock_long_short[stock.figi] = stock
-    fill_candles(client, stock_long_short)
+    get_market_stocks(client, all_stocks)
+    stocks = [s for s in all_stocks.values()]
+    now_stocks = stocks[120:150]
 
-    # if not os.path.isfile(path_stocks_usd):
-    #     get_market_stocks(client, stock_usd)
-    #     stocks_to_file(path_data_dir, path_stocks_usd, stock_usd)
-    # else:
-    #     stocks_from_file(path_data_dir, path_stocks_usd, stock_usd)
-    #
-    # for stock in stock_usd.values():
-    #     if stock.able_for_short:
-    #         stock_able_for_short[stock.figi] = stock
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        for stock in now_stocks:
+            executor.submit(stock.add_scraping_data())
+        for stock in now_stocks:
+            executor.submit(stock.get_tradingview_data())
 
-    # task_streaming = asyncio.create_task(test_streaming(keys['token_tinkoff_real'], stock_long_short))
-    await asyncio.create_task(print_stock(stock_long_short))
-    # asyncio.create_task(update_ema(stock_usd))
-    # asyncio.create_task(detect_intersection(stock_usd))
+    for s in now_stocks:
+        print('\n' + s.ticker)
+        for tf in (s.tv_hour, s.tv_day):
+            print(tf.interval)
+            print(tf.sum)
+            print(tf.ema)
+            print(tf.osc)
 
-    # await task_streaming
+    # for stock in stock_long_short.values():
+    #     this_stock = TA_Handler(
+    #         symbol=stock.ticker,
+    #         screener="america",
+    #         exchange=stock.data['exchange'],
+    #         interval=Interval.INTERVAL_1_DAY
+    #     )
+
+    # brent = TA_Handler(
+    #     symbol='UKOIL',
+    #     screener="cfd",
+    #     exchange='FX',
+    #     interval=Interval.INTERVAL_15_MINUTES
+    # )
 
 
 if __name__ == "__main__":
     start_time = time.time()
     asyncio.run(main())
-    print(f"Seconds: {time.time() - start_time:.3f}")
+    print(f"\n\nSeconds: {time.time() - start_time:.3f}")
