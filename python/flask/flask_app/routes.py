c5aae7126afeb387f74db5462fcced96192a3134
ActionLS <opil94@yandex.ru>
2021-11-26 15:24:58 +0300

    Add from sign in

--- python/flask/flask_app/routes.py
+++ python/flask/flask_app/routes.py
@@ -1,5 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from flask_app import app
+from flask_app.forms import LoginForm
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requisted for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
 
 
 @app.route('/')
