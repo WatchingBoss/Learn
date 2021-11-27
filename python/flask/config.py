7194d83137b083687f31b7b9c99257247045c7a5
ActionLS <opil94@yandex.ru>
2021-11-27 11:57:51 +0300

    Add model of db

--- python/flask/config.py
+++ python/flask/config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_is_hardcoded_string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f"sqlite///{os.path.join(basedir, 'app.db')}"
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f"sqlite///{os.path.join(basedir, 'app.db')}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f"sqlite:///{os.path.join(basedir, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
