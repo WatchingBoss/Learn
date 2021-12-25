010ed4916f3b5b9e32c0569454a8e081e234b3b6
ActionLS <opil94@yandex.ru>
2021-12-25 23:21:09 +0300

    Add profile and it's editor

--- python/flask/flask_app/forms.py
+++ python/flask/flask_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from flask_app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,37 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use different username. This username already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use different email address. This email already used.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use different username. This username already taken.')
--- python/flask/flask_app/routes.py
+++ python/flask/flask_app/routes.py
@@ -1,42 +1,90 @@
-from flask import render_template, flash, redirect, url_for
-from flask_app import app
-from flask_app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_app import app, db
+from flask_app.forms import LoginForm, RegistrationForm, EditProfileForm
+from flask_login import current_user, login_user, logout_user, login_required
+from flask_app.models import User
+from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/profile/<username>')
+@login_required
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Post body #1'},
+        {'author': user, 'body': 'Post body #2'},
+        {'author': user, 'body': 'Post body #3'}
+    ]
+    return render_template('profile.html', title='Profile', user=user, posts=posts)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(form.username.data, form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now registered user')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requisted for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'name': 'John',
-            'age': 25}
-
-    phones = [
-        {
-            'manufacturer': 'Xiaomi',
-            'model': 'Redmi 9',
-            'price': 12_490,
-            'currency': 'RUR'
-        },
-        {
-            'manufacturer': 'Apple',
-            'model': 'iPhone 11',
-            'price': 50_880,
-            'currency': 'RUR'
-        },
-        {
-            'manufacturer': 'Samsung',
-            'model': 'Galaxy A12',
-            'price': 11_590,
-            'currency': 'RUR'
-        }
-    ]
-
-    return render_template('index.html', title='Home', user=user, phones=phones)
+    # return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home')
