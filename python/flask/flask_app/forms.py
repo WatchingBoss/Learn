058afa74839325fe83f2ba717d891272a55263d2
ActionLS <opil94@yandex.ru>
2021-12-29 16:07:21 +0300

    Write new post

--- python/flask/flask_app/forms.py
+++ python/flask/flask_app/forms.py
@@ -47,3 +47,8 @@ class EditProfileForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Write something', validators=[DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField('Submit')
