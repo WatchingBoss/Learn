a1ec0dacc226f0b45a2b9ab2c4abc9a2b62fed4d
ActionLS <opil94@yandex.ru>
2022-01-16 14:32:02 +0300

    Change file

--- python/learning_1/compare_threading.py
+++ python/learning_1/compare_threading.py
@@ -13,16 +13,6 @@ def factorial(x):
     print(f"Factorial of {x:-5} is {result}")
 
 
-def return_factorial(x):
-    if x == 0 or x == 1:
-        return 1
-    result = 1
-    for i in range(x, 1, -1):
-        result *= i
-    time.sleep(0.25)
-    return f"Factorial of {x:-5} is {result}"
-
-
 def do_queue(start, end):
     for i in range(start, end):
         factorial(i**2)
@@ -42,7 +32,7 @@ def do_threading(start, end):
 
 def do_concurrent(start, end):
     with concurrent.futures.ThreadPoolExecutor() as ex:
-        results = [ex.submit(return_factorial, i**2) for i in range(start, end)]
+        results = [ex.submit(factorial, i**2) for i in range(start, end)]
         for i in concurrent.futures.as_completed(results):
             print(i.result())
 
@@ -51,17 +41,17 @@ def compare(func):
     start = time.perf_counter()
     x = 5
     func(x, x+5)
-    finish = time.perf_counter()
+    duration = time.perf_counter() - start
 
-    print(f"Used time: {finish-start:.5} seconds\n")
+    print(f"Used time: {duration:.5} seconds\n")
 
 
-if __name__ == "__main__":
-    print("Do queue")
-    compare(do_queue)
+# Run functions here
+print("Do queue")
+compare(do_queue)
 
-    print("Do threading")
-    compare(do_threading)
+print("Do threading")
+compare(do_threading)
 
-    print("Do concurrent")
-    compare(do_concurrent)
+print("Do concurrent")
+compare(do_concurrent)
